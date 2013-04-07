9849855ee386582905be70494310428d8e582ee0
wancharle <wancharle@gmail.com>
2013-04-07 05:28:50 -0300

    organizando libs para um unico arquivos

--- site/js/exemplos.js
+++ site/js/exemplos.js
@@ -1,37 +1,113 @@
+/* This file was auto-generated using RapydScript */
+(function(){
+var markers = new L.MarkerClusterGroup();
+var markersList = [];
+function populate() {
+    for (var i = 0; i < 400; i++) {
+        var m = new L.Marker(getRandomLatLng(map2));
+        markersList.push(m);
+        markers.addLayer(m);
+    }
+    return false;
+}
+function populateRandomVector() {
+    for (var i = 0, latlngs = [], len = 20; i < len; i++) {
+        latlngs.push(getRandomLatLng(map2));
+    }
+    var path = new L.Polyline(latlngs);
+    map2.addLayer(path);
+}
+function getRandomLatLng(map) {
+    var bounds = map.getBounds(),
+        southWest = bounds.getSouthWest(),
+        northEast = bounds.getNorthEast(),
+        lngSpan = northEast.lng - southWest.lng,
+        latSpan = northEast.lat - southWest.lat;
 
-var urlosm = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-    attribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>';
+    return new L.LatLng(
+            southWest.lat + latSpan * Math.random(),
+            southWest.lng + lngSpan * Math.random());
+}
 
-// marcadores
-var UFES = [-20.277233,-40.303752 ], 
-    CT = [-20.273530, -40.305448],
-    CEMUNI = [ -20.279483,-40.302690],
-    BIBLIOTECA = [-20.276519, -40.304503];
+var map2;
+function exemplo_markercluster(){
+		var Url = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+			Attribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery  <a href="http://cloudmade.com">CloudMade</a>',		
+            cloudmade = new L.TileLayer(Url, {maxZoom: 18, attribution: Attribution}),
+			latlng = new L.LatLng(-20.277233,-40.303752 );
 
-function exemplo1() {
+		 map2 = new L.Map('map_cluster', {center: latlng, zoom: 15, layers: [cloudmade]});
+
+	
+
+
+		populate();
+		map2.addLayer(markers);
+        
+        map2.setZoom(14);
+}
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheet/pub?key=0AhU-mW4ERuT5dHBRcGF5eml1aGhnTzl0RXh3MHdVakE&single=true&gid=0&output=html';
+
+ 
+
+var map_gdoc;
+function exemplo_gdoc(){
+
+          Tabletop.init( { key: public_spreadsheet_url,
+                         callback: showInfo,
+                         simpleSheet: true } );
+   
    var CamadaBasica = L.tileLayer(urlosm,  { attribution: attribution, maxZoom: 18 })
    
-   var  markers = [L.marker(CT).bindPopup('CT.'),
-        L.marker(CEMUNI).bindPopup('CEMUNI'), 
-        L.marker(BIBLIOTECA).bindPopup('Biblioteca')];
-   var centros = L.layerGroup(markers);     
    
-   var map = L.map('map',{layers:[CamadaBasica,centros],center: UFES,zoom: 15});
+   map_gdoc = L.map('map_gdoc',{layers:[CamadaBasica],center: UFES,zoom: 15});
 
-    L.marker(UFES).addTo(map)
-        .bindPopup('UFES')
-        .openPopup();
+    
 
-    var baseMaps = {
-    };
-
-    var overlayMaps = {
-        "Centros": centros
-    };
-
-    L.control.layers(baseMaps, overlayMaps).addTo(map);
 
  }
+
+function showInfo(data) {
+        for (i=0;i<data.length;i++){
+             var p =  [parseFloat(data[i].latitude.replace(',','.')), 
+                       parseFloat(data[i].longitude.replace(',','.'))]; 
+            L.marker(p).addTo(map_gdoc).bindPopup(data[i].textomarcador);
+        }
+}
+
+var map_geo;
+function add_iti(data){
+        var markerLocation = new L.LatLng(data.features[0].centroid.coordinates[1], data.features[0].centroid.coordinates[0]);
+	    L.marker(markerLocation).addTo(map_geo);  
+    
+       // console.info(markerLocation); 
+    }
+function exemplo_geocoding() {
+   var CamadaBasica = L.tileLayer(urlosm,  { attribution: attribution, maxZoom: 18 })
+ 
+   map_geo = L.map('map_geo',{layers:[CamadaBasica],center: UFES,zoom: 15});
+   L.marker(UFES).addTo(map_geo)
+        .bindPopup('UFES')
+         .openPopup();
+
+    geoCode("street:Av. Fernando Ferrari;city:vitoria","vitoria",add_iti);
+    geoCode("street:BR-101 NORTE;city:serra:",'',add_iti);
+    geoCode("street:RUA sao domingos;city:serrA",'BR-101 NORTE',add_iti);
+
+   
+}
+
+function geoCode(address, centro, callback) {
+	var firstPart = 'http://geocoding.cloudmade.com/bbcf9165c23646efbb1828828278c8bc/geocoding/v2/find.geojs?query='
+	var url = firstPart + encodeURI(address)
+	$.ajax({
+	  url: url,
+	  success: callback,
+      type:"POST",
+      dataType: 'jsonp',
+	})
+}
+ 
 
 var ultimo_zoom = 5; // para comecar dando zoom out
 function exemplo2() {
@@ -60,11 +136,44 @@ function exemplo2() {
 
  }
 
-function exemplos(){
-    exemplo1();    
-    exemplo2();
-    exemplo_markercluster();
-    exemplo_gdoc();
-    exemplo_geocoding();
-}
 
+urlosm = "http://{s}.tile.osm.org/{z}/{x}/{y}.png";
+attribution = "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery \u00a9 <a href=\"http://cloudmade.com\">CloudMade</a>";
+UFES = [(-20.277233), (-40.303752)];
+CT = [(-20.27353), (-40.305448)];
+CEMUNI = [(-20.279483), (-40.30269)];
+BIBLIOTECA = [(-20.276519), (-40.304503)];
+exemplo1 = function() {
+  var CamadaBasica, baseMaps, centros, map, markers, overlayMaps;
+  
+  CamadaBasica = L.tileLayer(urlosm, {
+    attribution: attribution,
+    maxZoom: 18
+  });
+  markers = [L.marker(CT).bindPopup("CT."), L.marker(CEMUNI).bindPopup("CEMUNI"), L.marker(BIBLIOTECA).bindPopup("Biblioteca")];
+  centros = L.layerGroup(markers);
+  map = L.map("map", {
+    layers: [CamadaBasica, centros],
+    center: UFES,
+    zoom: 15
+  });
+  L.marker(UFES).addTo(map).bindPopup("UFES").openPopup();
+  baseMaps = {
+    
+  };
+  overlayMaps = {
+    "Centros": centros
+  };
+  L.control.layers(baseMaps, overlayMaps).addTo(map);
+};
+
+exemplos = function() {
+  exemplo1();
+  exemplo2();
+  exemplo_markercluster();
+  exemplo_gdoc();
+  exemplo_geocoding();
+};
+
+
+}());
--- site/js/searchlight.py
+++ site/js/searchlight.py
@@ -1,4 +1,17 @@
+#imports de arquivos javascripts crus
+import libs.leaflet05
+L.Icon.Default.imagePath = "images/leaflet"
+import libs.leaflet.markercluster.markercluster
+#import libs.spin
+#import libs.leaflet.spin
+
+import libs.jquery191min
+import libs.jquery.getUrlParam
+import libs.tabletop
+
+#import normais
 import stdlib
+
 
 urlosm = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
 attribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
@@ -11,6 +24,8 @@ BIBLIOTECA = [-20.276519, -40.304503]
 
 public_spreadsheet_url = 'https://docs.google.com/spreadsheet/pub?key=0AhU-mW4ERuT5dHBRcGF5eml1aGhnTzl0RXh3MHdVakE&single=true&gid=0&output=html'
 map_gdoc= None
+portoalegrecc_json = "http://portoalegre.cc/causes/visibles?topLeftY=-29.993308319952344&topLeftX=-51.05793032165525&bottomRightY=-30.127023880027313&bottomRightX=-51.34906801696775&currentZoom=1&maxZoom=6"
+pacc_jsonp = "https://dl.dropbox.com/u/877911/portoalegre.js"
 
 def teste():
     alert("ola")
@@ -21,13 +36,21 @@ def main():
     url = decodeURIComponent(getURLParameter("data"))
     Tabletop.init( { 'key': url, 'callback': showInfo, 'simpleSheet': true } )
     CamadaBasica = L.tileLayer(urlosm,  { attribution: attribution, maxZoom: 18 })
+    getJson(pacc_jsonp, searchlight_callback)
+
     map_gdoc = L.map('map_gdoc',{layers:[CamadaBasica],center: UFES,zoom: 15})
 
 def showInfo(data):
     nonlocal urlosm, attribuition, UFES, public_spreadsheet_url, map_gdoc
     for d in data:
-       p =  [parseFloat(d.latitude.replace(',','.')), parseFloat(d.longitude.replace(',','.'))]; 
-       L.marker(p).addTo(map_gdoc).bindPopup(d.textomarcador);
+       p =  [parseFloat(d.latitude.replace(',','.')), parseFloat(d.longitude.replace(',','.'))] 
+       L.marker(p).addTo(map_gdoc).bindPopup(d.textomarcador)
 
 def getURLParameter(name):
-    return $(document).getUrlParam(name);
+    return $(document).getUrlParam(name)
+
+def searchlight_callback(data):
+    alert(data[0].cause.latitude);
+
+def getJson(url,func):
+	$.ajax({ 'url': url, 'success': func, 'type':"POST", 'dataType': 'jsonp'})
